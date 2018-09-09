4cc15d0f2c27acd5fede52267ac7ebfc111c1132
emmanuelsantiagoaponte <emmanuelsantiagoaponte@10.34.0.6>
2018-09-09 16:29:49 -0400

    new method for withdrawl.

--- Study Session/src/Register.java
+++ Study Session/src/Register.java
@@ -1,31 +1,42 @@
 
 public class Register {
-	
-		private double balance;
-		private double inputedBalance;
-		private double inputedWithdrawl;
-		
-		public Register(double userBalance) {
-			
-			balance = userBalance;
-					
-		}
-		
-		public Register() {
-			
-			balance = 0;
-									
-		}
-		
-		public void deposit(double depositedAmount) {
-			
-			balance += depositedAmount;
-		}
-		
-		public double getBalance() {
-			return balance;
-		}
-		
-		
-		
-}
+
+	private double balance;
+	private double inputedBalance;
+	private double inputedWithdrawl;
+
+	public Register(double userBalance) {
+
+		balance = userBalance;
+
+	}
+
+	public Register() {
+
+		balance = 0;
+
+	}
+
+	public void deposit(double depositedAmount) {
+
+		balance += depositedAmount;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void withdrawl(double desiredAmount) {
+
+		balance -= desiredAmount;
+
+
+
+	}
+
+
+	}
+
+
+
+
